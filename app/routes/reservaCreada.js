7be3a68cde7cfcf079b315e5e60ca059903e21fb
Juan Ramirez Catrileo <97692213+JuanRamirezCatrileo@users.noreply.github.com>
2022-07-11 16:29:44 -0400

    add GET params numero_reserva and const

--- app/routes/reservaCreada.js
+++ app/routes/reservaCreada.js
@@ -2,6 +2,8 @@ const express= require('express')
 
 const controller = require('../controllers/reservaCreada')
 
+const reservaCreada = require('../models/reservaCreada')
+
 const router = express.Router()
 
 const {validateReservaCreada} = require('../validators/reservaCreada')
@@ -12,6 +14,25 @@ router.get(
     `/${path}`,
     controller.getData
 )
+
+router.get('/reservacreada2', function(req, res) {
+    let numero_reserva = req.query.numero_reserva;
+    
+    reservaCreada.find({ numero_reserva: numero_reserva}, function(err, dbConnect) {
+        if (err) {
+            return res.json({
+                success: false,
+                msj: 'No se encontraron los datos',
+                err
+            });
+        } else {
+            return res.json({
+                success: true,
+                reserva: dbConnect
+            });
+        }
+    })
+});
 
 router.post(
     `/${path}`, validateReservaCreada,
