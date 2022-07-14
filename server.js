eb5b31b7cec009bfaee06d817f851f3ba02593fd
Nelson-pb <107379664+Nelson-pb@users.noreply.github.com>
2022-07-13 23:27:23 -0400

    actualizacion bd

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express')
 require("dotenv").config()
 const cors = require("cors")
 const app = express()
-const userRouters = require('./app/routes/user')
-const registroRouters = require('./app/routes/registro')
+const userRouters = require('./app/routes/login')
 const reservaRouters = require('./app/routes/reserva')
 const reservaCreada = require('./app/routes/reservaCreada')
 const servicio = require('./app/routes/servicio')
@@ -25,7 +24,6 @@ app.use(
 )
 app.use(cors())
 app.use(userRouters)
-app.use(registroRouters)
 app.use(reservaRouters)
 app.use(reservaCreada)
 app.use(servicio)
@@ -39,3 +37,6 @@ const port = process.env.PORT || 3000
 app.listen(port, function() {
     console.log("App  lista en", port)
 })
+
+
+
