f76ca6bf6c1370f64d10edcfee94712a573f1fb0
Nelson-pb <107379664+Nelson-pb@users.noreply.github.com>
2022-07-14 23:33:06 -0400

    busqueda por parametros

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,6 +1,7 @@
 const express= require('express')
 
 const controller = require('../controllers/login')
+const login = require('../models/login')
 
 const router = express.Router()
 
@@ -12,6 +13,25 @@ router.get(
     `/${path}`,
     controller.getData
 )
+
+router.get('/login2', function(req, res) {
+    let correo = req.query.correo;
+    let password = req.query.password;
+    login.find({ correo: correo, password }, function(err, dbConnect) {
+        if (err) {
+            return res.json({
+                success: false,
+                msj: 'No se encontraron los datos',
+                err
+            });
+        } else {
+            return res.json({
+                success: true,
+                login: dbConnect
+            });
+        }
+    })
+});
 
 router.post(
     `/${path}`, validateCreate,
